9f8c6e7ba5f299b1e17cdd8282d8a8787d7d5d4d
Startsev <tozarin@mail.ru>
2023-01-29 14:00:30 +0300

    Updates ast
    Signed-off-by: Startsev <tozarin@mail.ru>

--- lib/ast.ml
+++ lib/ast.ml
@@ -1,4 +1,4 @@
 type var = string [@@deriving show { with_path = false }]
 
-type expr = Abs of var * expr | App of expr * expr | Var of var
+type expr = Abs of var list * expr | App of expr * expr | Var of var
 [@@deriving show { with_path = false }]
