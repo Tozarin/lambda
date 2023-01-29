db53b3e0ed0d45fb1bc6ac4a84f866fb0b725c6e
Startsev <tozarin@mail.ru>
2023-01-29 14:17:48 +0300

    Adds cons for vars
    Signed-off-by: Startsev <tozarin@mail.ru>

--- lib/ast.ml
+++ lib/ast.ml
@@ -1,4 +1,4 @@
-type var = string [@@deriving show { with_path = false }]
+type var = Var of string [@@deriving show { with_path = false }]
 
 type expr = Abs of var list * expr | App of expr * expr | Var of var
 [@@deriving show { with_path = false }]
