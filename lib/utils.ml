c54f62f17059d9fd941f3c64d0666bd331953df5
Startsev <tozarin@mail.ru>
2023-05-11 01:21:44 +0300

    Adds new utils
    Signed-off-by: Startsev <tozarin@mail.ru>

--- lib/utils.ml
+++ lib/utils.ml
@@ -4,3 +4,9 @@
 
 let l_remove_str x list = List.filter (fun y -> not @@ String.equal x y) list
 let l_cont_str x list = List.exists (fun y -> String.equal x y) list
+
+let explode s =
+  let rec exp i l = if i < 0 then l else exp (i - 1) (s.[i] :: l) in
+  exp (String.length s - 1) []
+
+let implode x = String.of_seq (List.to_seq x)
